b0d45b114ab61c9d4a3f6b8926c39755e82c768d
hsucoder <hsuthazin255781@gmail.com>
2026-02-08 23:23:30 +0630

    add spring profile

---
+++ spring-profile/card/card/src/main/java/com/mtk/card/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.mtk.card.repository;
+
+import com.mtk.card.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
